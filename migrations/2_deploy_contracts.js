5e860814d5f3f32a4a695f4e0f8458fadce46fe6
Odenigbo Godwin <godwintrav@gmail.com>
2022-08-02 21:14:04 +0000

    added deploy contracts migration file

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Dai = artifacts.require('mocks/Dai.sol');
+const Rep = artifacts.require('mocks/Rep.sol');
+const Zrx = artifacts.require('mocks/Zrx.sol');
+const Bat = artifacts.require('mocks/Bat.sol');
+const Dex = artifacts.require('Dex.sol');
