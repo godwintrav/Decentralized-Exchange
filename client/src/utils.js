c7b2f097a45162572d3997b9b0d368d1db9caede
Odenigbo Godwin <godwintrav@gmail.com>
2022-08-03 22:11:57 +0000

    fixed node-sass bug

---
+++ client/src/utils.js
@@ -0,0 +1,50 @@
+import Web3 from 'web3';
+import Dex from './contracts/Dex.json';
+import ERC20Abi from './ERC20Abi.json';
+
+const getWeb3 = () =>
+
+new Promise( async (resolve, reject) => {
+    
+    let provider = await detectEthereumProvider();
+    
+    if(provider) {
+        
+        await provider.request({ method: 'eth_requestAccounts' });
+        
+        try {
+            
+            const web3 = new Web3(window.ethereum);
+            
+            resolve(web3);
+            
+        } catch(error) {
+            
+            reject(error);
+            
+        }
+        
+    } reject('Install Metamask');
+    
+});
+
+const getContracts = async web3 => {
+    const networkId = await web3.eth.net.getId();
+    const  contractDeployment = Dex.networks[networkId];
+    const dex = new web3.eth.Contract(Dex.abi, contractDeployment.address);
+
+    const tokens = await dex.methods.getTokens().call();
+  const tokenContracts = tokens.reduce((acc, token) => (
+    {
+    ...acc,
+    [web3.utils.hexToUtf8(token.ticker)]: new web3.eth.Contract(
+      ERC20Abi,
+      token.tokenAddress
+    )
+  }
+  ), {});
+  
+  return { dex, ...tokenContracts };
+};
+
+export { getWeb3, getContracts };
