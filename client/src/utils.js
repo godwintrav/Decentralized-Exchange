8140354c0baed8ae40ecc3ce3ab303bb55c483f6
Odenigbo Godwin <godwintrav@gmail.com>
2022-08-04 09:36:16 +0000

    added LoadingComponent

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,31 +2,41 @@ import Web3 from 'web3';
 import Dex from './contracts/Dex.json';
 import ERC20Abi from './ERC20Abi.json';
 
-const getWeb3 = () =>
-
-new Promise( async (resolve, reject) => {
-    
-    let provider = await detectEthereumProvider();
-    
-    if(provider) {
-        
-        await provider.request({ method: 'eth_requestAccounts' });
-        
-        try {
-            
-            const web3 = new Web3(window.ethereum);
-            
+const getWeb3 = () => {
+    return new Promise((resolve, reject) => {
+      // Wait for loading completion to avoid race conditions with web3 injection timing.
+      window.addEventListener("load", async () => {
+        // Modern dapp browsers...
+        if (window.ethereum) {
+          const web3 = new Web3(window.ethereum);
+          try {
+            // Request account access if needed
+            await window.ethereum.enable();
+            // Acccounts now exposed
             resolve(web3);
-            
-        } catch(error) {
-            
+          } catch (error) {
             reject(error);
-            
+          }
         }
-        
-    } reject('Install Metamask');
-    
-});
+        // Legacy dapp browsers...
+        else if (window.web3) {
+          // Use Mist/MetaMask's provider.
+          const web3 = window.web3;
+          console.log("Injected web3 detected.");
+          resolve(web3);
+        }
+        // Fallback to localhost; use dev console port by default...
+        else {
+          const provider = new Web3.providers.HttpProvider(
+            "http://localhost:9545"
+          );
+          const web3 = new Web3(provider);
+          console.log("No web3 instance injected, using Local web3.");
+          resolve(web3);
+        }
+      });
+    });
+  };
 
 const getContracts = async web3 => {
     const networkId = await web3.eth.net.getId();
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -80,7 +81,7 @@ module.exports = {
   mocha: {
     timeout: false
   },
-
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   // Configure your compilers
   compilers: {
     solc: {
